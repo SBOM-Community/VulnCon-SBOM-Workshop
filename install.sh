17ee7060e8cbf2a43c81324ce2ef99dd54768633
Ian Dunbar-Hall <ian.dunbar-hall@lmco.com>
2025-09-16 20:23:23 -0700

    adding codespace files
    Signed-off-by: Ian Dunbar-Hall <ian.dunbar-hall@lmco.com>

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 brew install trivy
 brew tap interlynk-io/interlynk
